b873ae6660112fa87a08b832f2b5a3ced2a47908
htdat0704 <hodat402@gmail.com>
2023-07-21 16:57:11 +0700

    -added

--- frontend/src/components/testChrist/CommentVideo.js
+++ frontend/src/components/testChrist/CommentVideo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
@@ -11,35 +11,91 @@ const CommentVideo = ({ socket }) => {
   });
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [watchingCount, setWatchingCount] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
+  const [cmTimeRangeStartInSecond, setTimeRangeStartInSecond] = useState(0);
 
-  const updateCommentList = () => {
-    socket.on("comments updated", (data) => {
-      setComments(
-        data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-      );
-      setLoading(false);
-    });
-  };
   useEffect(() => {
     if (!localStorage.getItem("userName")) {
       navigate("/");
     }
 
-    socket.emit("getComments", { videoID: id });
-    socket.emit("join", { room: id, user: localStorage.getItem("userName") });
-    updateCommentList();
+    socket.emit("getComments", {
+      videoID: id,
+      queryParams: {
+        page,
+        pageSize,
+      },
+      searchOptions: {
+        cmTimeRangeStartInSecond,
+      },
+    });
+    socket.emit("join", {
+      roomID: id,
+      userID: localStorage.getItem("userName"),
+      socketID: socket.id,
+    });
+    socket.on("comments updated", (data) => {
+      setComments(data.result.items);
+      setLoading(false);
+    });
+
+    socket.on("comment created", (data) => {
+      setComments((prev) => [data, ...prev]);
+    });
+
+    socket.on("comment reacted", (data) => {
+      setComments((prev) =>
+        prev.map((comment) => (comment._id === data._id ? data : comment))
+      );
+    });
+
+    socket.on("watching count", (data) => {
+      setWatchingCount(data);
+    });
+
+    socket.on("disconnect");
+
+    return () => {
+      socket.off("comments updated");
+      socket.off("comment created");
+      socket.off("comment reacted");
+      socket.off("disconnect");
+    };
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     socket.emit("comment", {
       ...comment,
       username: localStorage.getItem("userName"),
       videoID: id,
     });
+  };
 
-    updateCommentList();
+  const handleMore = () => {
+    fetch(
+      `http://localhost:4002/api/v1/comment/${id}?` +
+        new URLSearchParams({
+          page: page + 1,
+          pageSize,
+          cmTimeRangeStartInSecond,
+        })
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setComments((prev) => [
+          ...prev,
+          ...res.data.result.items.filter(
+            (comment) =>
+              !prev.some((commentPrev) => commentPrev._id === comment._id)
+          ),
+        ]);
+        setLoading(false);
+      });
+
+    setPage(page + 1);
   };
 
   const handleReact = (commentID) => {
@@ -47,8 +103,6 @@ const CommentVideo = ({ socket }) => {
       commentID,
       videoID: id,
     });
-
-    updateCommentList();
   };
 
   const handleOnChange = (e) => {
@@ -64,10 +118,16 @@ const CommentVideo = ({ socket }) => {
     }
   };
 
+  const handleBack = () => {
+    socket.emit("disconnectRoom");
+    navigate("/videos");
+  };
+
   return (
     <div>
       <div className="bidproduct__container">
-        <Link to="/videos">Back</Link>
+        <button onClick={() => handleBack()}>Back</button>
+        <h2>Watching Now : {watchingCount}</h2>
         <h2>Comment a Video</h2>
         <form className="bidProduct__form" onSubmit={handleSubmit}>
           Title:
@@ -121,6 +181,9 @@ const CommentVideo = ({ socket }) => {
           </tbody>
         </table>
       </div>
+      <button className="bidProduct__cta" onClick={() => handleMore()}>
+        More
+      </button>
     </div>
   );
 };
